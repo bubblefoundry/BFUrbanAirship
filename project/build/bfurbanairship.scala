ca30c2e2565f2faade0443128b3bd016cfbdbf94
Peter Robinett <peter@bubblefoundry.com>
2011-02-06 23:24:20 +0100

    Update to Lift 2.3-SNAPSHOT to be on the same version as the iFebo push app.

--- project/build/bfurbanairship.scala
+++ project/build/bfurbanairship.scala
@@ -4,7 +4,10 @@ class BFUrbanAirshipProject(info: ProjectInfo) extends DefaultProject(info)
 {
   // val dispatch_json = "net.databinder" %% "dispatch-json" % "0.7.8"
   // val dispatch_lift_json = "net.databinder" %% "dispatch-lift-json" % "0.7.8"
+  val scalatoolsSnapshot = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
+  val liftVersion = "2.3-SNAPSHOT"
+  
   val dispatch = "net.databinder" %% "dispatch" % "0.7.8"
-  val lift_common = "net.liftweb" %% "lift-common" % "2.2"
-  val lift_util = "net.liftweb" %% "lift-util" % "2.2"
+  val lift_common = "net.liftweb" %% "lift-common" % liftVersion
+  val lift_util = "net.liftweb" %% "lift-util" % liftVersion
 }
